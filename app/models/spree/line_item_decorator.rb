8317a0c095731af9a5f81e2b8e337f925395035a
Ruben Ascencio <galateaweb@gmail.com>
2014-01-28 14:37:22 -0800

    Do not nullify so we can get children before destroying

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,6 +1,6 @@
 Spree::LineItem.class_eval do
   belongs_to :parent, :class_name => "Spree::LineItem", :foreign_key => :parent_id
-  has_many :children, :class_name => "Spree::LineItem", :foreign_key => :parent_id, :dependent => :nullify
+  has_many :children, :class_name => "Spree::LineItem", :foreign_key => :parent_id
   has_many :inventory_units, inverse_of: :line_item
   before_destroy :update_inventory
 
