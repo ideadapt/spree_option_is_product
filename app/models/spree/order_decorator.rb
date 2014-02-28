4ad5a6228ed15943417805f85b3bcb22f24ffaab
Ruben Ascencio <galateaweb@gmail.com>
2014-02-27 18:00:22 -0800

    Sort line_items by parent then children

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,4 +1,7 @@
 Spree::Order.class_eval do
+
+  has_many :line_items, -> { order('COALESCE(spree_line_items.parent_id, spree_line_items.id), spree_line_items.parent_id IS NOT NULL, spree_line_items.id') }, dependent: :destroy
+
   def find_line_item_by_variant(variant)
     line_items.detect { |line_item| (line_item.variant_id == variant.id) && line_item.parent.blank? && line_item.children.blank? }
   end
