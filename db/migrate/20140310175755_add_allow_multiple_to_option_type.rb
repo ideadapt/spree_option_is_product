6ea1b044889ebbf303f8b09e4d40accd68fbef37
ideadapt <kunz@ideadapt.net>
2014-04-02 11:31:32 +0200

    Update 20140310175755_add_allow_multiple_to_option_type.rb

--- db/migrate/20140310175755_add_allow_multiple_to_option_type.rb
+++ db/migrate/20140310175755_add_allow_multiple_to_option_type.rb
@@ -1,5 +1,5 @@
 class AddAllowMultipleToOptionType < ActiveRecord::Migration
   def change
-    add_column :spree_option_types, :allow_multiple, :boolean, default: 0
+    add_column :spree_option_types, :allow_multiple, :boolean, default: false
   end
 end
